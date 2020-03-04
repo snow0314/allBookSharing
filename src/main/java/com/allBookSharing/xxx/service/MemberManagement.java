303a7a6df6f1816938b6824352c2d4d14fcb0ff9
djsk46 <djsk46@naver.com>
2020-03-04 17:13:23 +0900

    오후 5시 13분

--- src/main/java/com/allBookSharing/xxx/service/MemberManagement.java
+++ src/main/java/com/allBookSharing/xxx/service/MemberManagement.java
@@ -1,5 +1,24 @@
 package com.allBookSharing.xxx.service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+@Service
 public class MemberManagement {
+
+	private ModelAndView mav=new ModelAndView();
+	
+	public ModelAndView showWishList(HttpServletRequest req) {
+		mav = new ModelAndView();
+		
+		String id = (String) req.getSession().getAttribute("id");
+		System.out.println("나의 아이디는 "+id);
+		
+		
+		mav.setViewName("checkedlist");
+		return mav;
+	}
 
 }
