c5e7b6c4520502faa5d9aa794e9645f49ef4d411
bear21678 <59427138+bear21678@users.noreply.github.com>
2020-03-04 16:58:11 +0900

    03/04 5시

--- src/main/java/com/allBookSharing/xxx/HomeController.java
+++ src/main/java/com/allBookSharing/xxx/HomeController.java
@@ -1,25 +1,46 @@
 package com.allBookSharing.xxx;
 
-
 import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	@Autowired
+
+	private ModelAndView mav;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
-		
+
 		return "home";
 	}
-	
+
+	@PreAuthorize("isAnonymous()") // 로그인 안했을때 접근 불가
+	@RequestMapping(value = "/joinfrm", method = RequestMethod.GET)
+	public ModelAndView joinFrm() {
+		mav = new ModelAndView();
+		mav.setViewName("joinFrm");
+
+		return mav;
+	}
+
+	// 회원가입
+	@RequestMapping(value = "/memberjoin", method = RequestMethod.POST)
+	public ModelAndView memberjoin(Member mb) {
+
+		mav = mm.memberjoin(mb);
+
+		return mav;
+	}
+
 }
